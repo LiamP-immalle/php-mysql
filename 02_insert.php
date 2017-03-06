1c0f8eb103f50d14c0d120835008cb177988f0b6
LiamP-immalle <liam.present@immalle.be>
2017-03-06 09:31:04 +0100

    Oefening 02_insert.php werkt

--- 02_insert.php
+++ 02_insert.php
@@ -8,8 +8,8 @@ include 'db_credentials.php';
 // dit niet persé in php te doen.
 
 function voegBerichtToe($conn, $inhoud) {
-    $sql = "INSERT INTO ... 
-        VALUES('$inhoud', now())";
+    $sql = "INSERT INTO messages ( inhoud, tijdstip )
+        VALUES($inhoud', now())";
     $conn->exec($sql);
 }
 
